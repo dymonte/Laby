1c4452ade518aa31a7a8db9b7cea11b8d5068233
g-imhoff <guillaume.imhoff170804@gmail.com>
2024-11-08 13:45:48 +0100

    début d'ajout de a*

--- src/generation.c
+++ src/generation.c
@@ -1,91 +1,74 @@
+#include "../deps/generation.h"
 #include <stdlib.h>
-#include "generation.h"
 
-Tab tab_start(int w, int h)
-{
-    Tab t = new_tab(w, h);
+Tab tab_start(int w, int h) {
+  Tab t = new_tab(w, h);
 
-    for (int i = t.start_y; i < h; i++)
-    {
-        t.cells[i][t.start_x].left = 0;
-        t.cells[i][t.start_x + w - 1].right = 0;
-    }
+  for (int i = t.start_y; i < h; i++) {
+    t.cells[i][t.start_x].left = 0;
+    t.cells[i][t.start_x + w - 1].right = 0;
+  }
 
-    for (int i = t.start_x; i < w; i++)
-    {
-        t.cells[t.start_y][i].up = 0;
-        t.cells[t.start_y + h - 1][i].down = 0;
-    }
+  for (int i = t.start_x; i < w; i++) {
+    t.cells[t.start_y][i].up = 0;
+    t.cells[t.start_y + h - 1][i].down = 0;
+  }
 
-    t.cells[t.height - 1][t.width - 1].type = end;
-    return t;
+  t.cells[t.height - 1][t.width - 1].type = end;
+  return t;
 }
 
-Tab generate(Tab tab, int orientation, int random_wall, int random_orientation)
-{
-    if (tab.height == 1 || tab.width == 1)
-        return tab;
-
-    Tab t1 = tab;
-    Tab t2 = tab;
-    if (orientation == 0)
-    {
-        int w = random_wall ? rand() % (tab.width - 1) + 1 : (int)(tab.width / 2);
-        int w_wall = w + tab.start_x;
-        int pos_ouverture = rand() % tab.height;
-
-        for (int i = tab.start_y; i < tab.start_y + tab.height; i++)
-        {
-            if (i == tab.start_y + pos_ouverture)
-            {
-                tab.cells[i][w_wall - 1].right = 1;
-                tab.cells[i][w_wall].left = 1;
-            }
-            else
-            {
-                tab.cells[i][w_wall - 1].right = 0;
-                tab.cells[i][w_wall].left = 0;
-            }
-        }
-
-        t1.width = w;
-        t2.width = tab.width - w;
-        t2.start_x += w;
-    }
-    else
-    {
-        int h = random_wall ? rand() % (tab.height - 1) + 1 : (int)(tab.height / 2);
-        int h_wall = h + tab.start_y;
-        int pos_ouverture = rand() % tab.width;
-
-        for (int i = tab.start_x; i < tab.start_x + tab.width; i++)
-        {
-            if (i == tab.start_x + pos_ouverture)
-            {
-                tab.cells[h_wall - 1][i].down = 1;
-                tab.cells[h_wall][i].up = 1;
-            }
-            else
-            {
-                tab.cells[h_wall - 1][i].down = 0;
-                tab.cells[h_wall][i].up = 0;
-            }
-        }
-
-        t1.height = h;
-        t2.height = tab.height - h;
-        t2.start_y += h;
-    }
-
-    if (random_orientation)
-    {
-        generate(t1, rand() % 2, random_wall, random_orientation);
-        generate(t2, rand() % 2, random_wall, random_orientation);
-    }
-    else
-    {
-        generate(t1, (orientation + 1) % 2, random_wall, random_orientation);
-        generate(t2, (orientation + 1) % 2, random_wall, random_orientation);
-    }
+Tab generate(Tab tab, int orientation, int random_wall,
+             int random_orientation) {
+  if (tab.height == 1 || tab.width == 1)
     return tab;
+
+  Tab t1 = tab;
+  Tab t2 = tab;
+  if (orientation == 0) {
+    int w = random_wall ? rand() % (tab.width - 1) + 1 : (int)(tab.width / 2);
+    int w_wall = w + tab.start_x;
+    int pos_ouverture = rand() % tab.height;
+
+    for (int i = tab.start_y; i < tab.start_y + tab.height; i++) {
+      if (i == tab.start_y + pos_ouverture) {
+        tab.cells[i][w_wall - 1].right = 1;
+        tab.cells[i][w_wall].left = 1;
+      } else {
+        tab.cells[i][w_wall - 1].right = 0;
+        tab.cells[i][w_wall].left = 0;
+      }
+    }
+
+    t1.width = w;
+    t2.width = tab.width - w;
+    t2.start_x += w;
+  } else {
+    int h = random_wall ? rand() % (tab.height - 1) + 1 : (int)(tab.height / 2);
+    int h_wall = h + tab.start_y;
+    int pos_ouverture = rand() % tab.width;
+
+    for (int i = tab.start_x; i < tab.start_x + tab.width; i++) {
+      if (i == tab.start_x + pos_ouverture) {
+        tab.cells[h_wall - 1][i].down = 1;
+        tab.cells[h_wall][i].up = 1;
+      } else {
+        tab.cells[h_wall - 1][i].down = 0;
+        tab.cells[h_wall][i].up = 0;
+      }
+    }
+
+    t1.height = h;
+    t2.height = tab.height - h;
+    t2.start_y += h;
+  }
+
+  if (random_orientation) {
+    generate(t1, rand() % 2, random_wall, random_orientation);
+    generate(t2, rand() % 2, random_wall, random_orientation);
+  } else {
+    generate(t1, (orientation + 1) % 2, random_wall, random_orientation);
+    generate(t2, (orientation + 1) % 2, random_wall, random_orientation);
+  }
+  return tab;
 }
--- src/tab.c
+++ src/tab.c
@@ -1,51 +1,45 @@
+#include "../deps/tab.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "tab.h"
 
-Tab new_tab(int width, int height)
-{
-    Tab tab;
-    tab.width = width;
-    tab.height = height;
+Tab new_tab(int width, int height) {
+  Tab tab;
+  tab.width = width;
+  tab.height = height;
 
-    tab.start_x = 0;
-    tab.start_y = 0;
+  tab.start_x = 0;
+  tab.start_y = 0;
 
-    tab.cells = (Cell **)malloc(sizeof(Cell *) * height);
-    for (int i = 0; i < height; i++)
-    {
-        tab.cells[i] = (Cell *)malloc(sizeof(Cell) * width);
-        for (int j = 0; j < width; j++)
-        {
-            tab.cells[i][j].up = 1;
-            tab.cells[i][j].down = 1;
-            tab.cells[i][j].left = 1;
-            tab.cells[i][j].right = 1;
-            tab.cells[i][j].type = normal;
-            tab.cells[i][j].visited = 0;
-        }
+  tab.cells = (Cell **)malloc(sizeof(Cell *) * height);
+  for (int i = 0; i < height; i++) {
+    tab.cells[i] = (Cell *)malloc(sizeof(Cell) * width);
+    for (int j = 0; j < width; j++) {
+      tab.cells[i][j].up = 1;
+      tab.cells[i][j].down = 1;
+      tab.cells[i][j].left = 1;
+      tab.cells[i][j].right = 1;
+      tab.cells[i][j].type = normal;
+      tab.cells[i][j].visited = 0;
     }
-    tab.cells[height - 1][width - 1].type = end;
-    return tab;
+  }
+  tab.cells[height - 1][width - 1].type = end;
+  return tab;
 }
 
-void free_tab(Tab tab)
-{
-    for (int i = 0; i < tab.height; i++)
-    {
-        free(tab.cells[i]);
-    }
-    free(tab.cells);
+void free_tab(Tab tab) {
+  for (int i = 0; i < tab.height; i++) {
+    free(tab.cells[i]);
+  }
+  free(tab.cells);
 }
 
-void print_tab(Tab tab)
-{
-    for (int i = tab.start_y; i < tab.height; i++)
-    {
-        for (int j = tab.start_x; j < tab.width; j++)
-            printf("%d %d %d %d   ", tab.cells[i][j].up, tab.cells[i][j].down, tab.cells[i][j].left, tab.cells[i][j].right);
-        printf("\n");
-    }
+void print_tab(Tab tab) {
+  for (int i = tab.start_y; i < tab.height; i++) {
+    for (int j = tab.start_x; j < tab.width; j++)
+      printf("%d %d %d %d   ", tab.cells[i][j].up, tab.cells[i][j].down,
+             tab.cells[i][j].left, tab.cells[i][j].right);
+    printf("\n");
+  }
 }
 
 /**
@@ -63,71 +57,55 @@ void print_tab(Tab tab)
  * The start_x and start_y fields of the Tab struct are taken into account
  * when printing the maze.
  */
-void maze_show(Tab t)
-{
-    for (int i = t.start_y; i < t.height; i++)
-    {
-        // Imprimer les murs du haut pour chaque cellule
-        for (int j = t.start_x; j < t.width; j++)
-        {
-            printf("+"); // coin supérieur gauche
-            if (t.cells[i][j].up)
-            {
-                printf("   "); // mur nord ouvert
-            }
-            else
-            {
-                printf("---"); // mur nord fermé
-            }
-        }
-        printf("+\n");
-
-        // Imprimer les murs de gauche et de droite pour chaque cellule
-        for (int j = t.start_x; j < t.width; j++)
-        {
-            if (t.cells[i][j].left)
-            {
-                printf(" "); // mur ouest ouvert
-            }
-            else
-            {
-                printf("|"); // mur ouest fermé
-            }
-
-            printf("%s", type_to_str(t.cells[i][j].type)); // espace pour la cellule
-        }
-        printf("|\n"); // mur est pour la dernière cellule
-    }
-
-    // Imprimer les murs du bas pour la dernière rangée
-    for (int j = t.start_x; j < t.width; j++)
-    {
-        printf("+");
-        if (t.cells[t.height - 1][j].down)
-        {
-            printf("   "); // mur sud ouvert
-        }
-        else
-        {
-            printf("---"); // mur sud fermé
-        }
+void maze_show(Tab t) {
+  for (int i = t.start_y; i < t.height; i++) {
+    // Imprimer les murs du haut pour chaque cellule
+    for (int j = t.start_x; j < t.width; j++) {
+      printf("+"); // coin supérieur gauche
+      if (t.cells[i][j].up) {
+        printf("   "); // mur nord ouvert
+      } else {
+        printf("---"); // mur nord fermé
+      }
     }
     printf("+\n");
+
+    // Imprimer les murs de gauche et de droite pour chaque cellule
+    for (int j = t.start_x; j < t.width; j++) {
+      if (t.cells[i][j].left) {
+        printf(" "); // mur ouest ouvert
+      } else {
+        printf("|"); // mur ouest fermé
+      }
+
+      printf("%s", type_to_str(t.cells[i][j].type)); // espace pour la cellule
+    }
+    printf("|\n"); // mur est pour la dernière cellule
+  }
+
+  // Imprimer les murs du bas pour la dernière rangée
+  for (int j = t.start_x; j < t.width; j++) {
+    printf("+");
+    if (t.cells[t.height - 1][j].down) {
+      printf("   "); // mur sud ouvert
+    } else {
+      printf("---"); // mur sud fermé
+    }
+  }
+  printf("+\n");
 }
 
-char *type_to_str(type_cell type)
-{
-    switch (type)
-    {
-    // case visited:
-    //     return " * ";
-    case start:
-        return " S ";
-    case end:
-        return " E ";
-    case path:
-        return " * ";
-    default:
-        return "   ";
-    }
+char *type_to_str(type_cell type) {
+  switch (type) {
+  // case visited:
+  //     return " * ";
+  case start:
+    return " S ";
+  case end:
+    return " E ";
+  case path:
+    return " * ";
+  default:
+    return "   ";
+  }
 }
