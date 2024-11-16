9cb80fc80a2163eb15f9a1b7ebe0535a3d6fc7c1
Dylan <dylan.monteneri@gmail.com>
2024-11-16 14:48:09 +0100

    modif gene stats

--- stats/generate_stats.sh
+++ stats/generate_stats.sh
@@ -1,14 +1,19 @@
-max=2000
+start=10000
+gap=200
+max=20000
+repeat=2
 gen=stats/result_gen.csv
 path=stats/result_path.csv
 
-for i in $(seq 1000 500 $max); do
-    printf "%d;%d;" $i $(($i * $i)) >>$gen
-    printf "%d;%d;" $i $(($i * $i)) >>$path
-    ./maze $i $i 0 0 1
-    echo >>$gen
-    echo >>$path
-    printf "%d fait\n" $i
+for i in $(seq $start $gap $max); do
+    for j in $(seq 1 $repeat); do
+        printf "%d;%d;" $i $(($i * $i)) >>$gen
+        printf "%d;%d;" $i $(($i * $i)) >>$path
+        ./maze $i $i 0 0 1
+        echo >>$gen
+        echo >>$path
+    done
+    printf "%d done\n" $i
 done
 
 python3 stats/generate_fig.py
